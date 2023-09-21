19bdc842946340aa59deafe52f42c6e58835c3ac
Nyndow <94800504+Nyndow@users.noreply.github.com>
2023-09-22 00:03:37 +0300

    Delete push.sh

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-username="your_username"
-password="your_password"
-
-git add .
-git commit -m "$(date)"  # Use the current date as the commit message
-echo -e "$username\n$password" | git push origin main
