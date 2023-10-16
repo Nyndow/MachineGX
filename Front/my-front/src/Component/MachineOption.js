b3d7f82a2da42101077dc8910b2be299d8982834
nyndow <nyantsatianany@gmail.com>
2023-10-16 03:58:36 +0300

    give form for the stepper

--- Front/my-front/src/Component/MachineOption.js
+++ Front/my-front/src/Component/MachineOption.js
@@ -22,7 +22,13 @@ function MachineOption() {
     nomOS: '',
     versionOS: '',
   });
+  const [userFormData, setUserFormData] = useState({
+    numEMP: '',
+    userUsername: '',
+    userPassword: '',
+  });
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [currentStep, setCurrentStep] = useState(0);
 
   useEffect(() => {
     fetchData();
@@ -47,12 +53,20 @@ function MachineOption() {
     }));
   }, []);
 
+  const handleUserInputChange = useCallback((event) => {
+    const { name, value } = event.target;
+    setUserFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }, []);
+
   const handleSubmit = useCallback(() => {
     if (!isSubmitting) {
       setIsSubmitting(true);
 
       axios
-        .post(`${apiUrl}/machine_user_add/`, formData)
+        .post(`${apiUrl}/machine_user_add/`, { ...formData, ...userFormData })
         .then((response) => {
           console.log('Data sent successfully', response);
         })
@@ -63,7 +77,23 @@ function MachineOption() {
           setIsSubmitting(false);
         });
     }
-  }, [apiUrl, isSubmitting, formData]);
+  }, [apiUrl, isSubmitting, formData, userFormData]);
+
+  const handleNext = () => {
+    if (currentStep < 1) {
+      setCurrentStep(currentStep + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentStep > 0) {
+      setCurrentStep(currentStep - 1);
+    }
+  };
+
+  const isFormEmpty = currentStep === 0
+    ? Object.values(formData).some((value) => value === '')
+    : Object.values(userFormData).some((value) => value === '');
 
   const nomOSOptions = useMemo(() => {
     // Create an array of unique nomOS values
@@ -89,107 +119,170 @@ function MachineOption() {
   return (
     <div className="machineOption-container">
       <div className="input-group">
-        <TextField
-          id="standard-basic"
-          label="Machine Name"
-          variant="standard"
-          name="machineName"
-          value={formData.machineName}
-          onChange={handleInputChange}
-          style={{ marginBottom: '10px' }}
-          required
-        />
-        <TextField
-          id="standard-basic"
-          label="IP Address"
-          variant="standard"
-          name="ipAddr"
-          value={formData.ipAddr}
-          onChange={handleInputChange}
-          style={{ marginBottom: '10px' }}
-          required
-        />
-        <TextField
-          id="standard-basic"
-          label="Port Number"
-          variant="standard"
-          name="portNumber"
-          value={formData.portNumber}
-          onChange={handleInputChange}
-          style={{ marginBottom: '10px' }}
-          required
-        />
-
-      <div className='select-OS'> 
-        <Box sx={{ minWidth: 120 }}>
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="nomOSSelect-label">OS</InputLabel>
-            <Select
-              labelId="nomOSSelect-label"
-              id="nomOSSelect"
-              name="nomOS"
-              value={formData.nomOS}
+        {currentStep === 0 && (
+          <>
+            <h3>Machine Information </h3>
+            <TextField
+              id="standard-basic"
+              label="Machine Name"
+              variant="standard"
+              name="machineName"
+              value={formData.machineName}
               onChange={handleInputChange}
-            >
-              {nomOSOptions}
-            </Select>
-          </FormControl>
-          </Box>
-
-          <Box sx={{ minWidth: 120 }}>
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="demo-simple-select-label">Version</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="versionOSSelect"
-              name="versionOS"
-              value={formData.versionOS}
+              style={{ marginBottom: '10px' }}
+              required
+            />
+            <TextField
+              id="standard-basic"
+              label="IP Address"
+              variant="standard"
+              name="ipAddr"
+              value={formData.ipAddr}
               onChange={handleInputChange}
+              style={{ marginBottom: '10px' }}
+              required
+            />
+            <TextField
+              id="standard-basic"
+              label="Port Number"
+              variant="standard"
+              name="portNumber"
+              value={formData.portNumber}
+              onChange={handleInputChange}
+              style={{ marginBottom: '10px' }}
+              required
+            />
+
+            <div className='select-OS'>
+              <Box sx={{ minWidth: 120 }}>
+                <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                  <InputLabel id="nomOSSelect-label">OS</InputLabel>
+                  <Select
+                    labelId="nomOSSelect-label"
+                    id="nomOSSelect"
+                    name="nomOS"
+                    value={formData.nomOS}
+                    onChange={handleInputChange}
+                    required
+                  >
+                    {nomOSOptions}
+                  </Select>
+                </FormControl>
+              </Box>
+
+              <Box sx={{ minWidth: 120 }}>
+                <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                  <InputLabel id="demo-simple-select-label">Version</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="versionOSSelect"
+                    name="versionOS"
+                    value={formData.versionOS}
+                    onChange={handleInputChange}
+                    required
+                  >
+                    {versionOSOptions}
+                  </Select>
+                </FormControl>
+              </Box>
+            </div>
+
+            <Link
+              fontSize="small"
+              color="info"
+              href="/add/oSys"
+              underline="hover"
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+              }}
             >
-              {versionOSOptions}
-            </Select>
-          </FormControl>
-        </Box>
-      </div>
-      <Link
-        fontSize="small"
-        color="info"
-        href="/add/oSys"
-        underline="hover"
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'flex-end', 
-        }}
-      >
-        <HelpIcon style={{ marginRight: '4px' }} />OS not there?
-      </Link>
+              <HelpIcon style={{ marginRight: '4px' }} />OS not there?
+            </Link>
 
-      <div style={{ display: 'flex', justifyContent: 'flex-end', paddingTop:'20px', paddingBottom:'15px' }}>
-      <Link
-        color="primary"
-        href="/add/oSys"
-        underline="none"
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center', 
-        }}
-      >Save without adding user
-      </Link>
-      </div>
+            <div style={{ display: 'flex', justifyContent: 'flex-end', paddingTop: '15px', paddingBottom: '15px' }}>
+              <Button
+                color="primary"
+                variant="text"
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                onClick={handleSubmit}
+                disabled={isFormEmpty} 
+              >
+                Save without adding user
+              </Button>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'flex-end'}}>
+              <Button
+                variant="contained"
+                color="success"
+                onClick={handleNext}
+                disabled={isFormEmpty} 
+              >
+                Next
+              </Button>
+            </div>
+          </>
+        )}
 
-      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <Button
-          variant="contained"
-          color="success"
-          onClick={handleSubmit}
-        >
-          Next
-        </Button>
-      </div>
+        {currentStep === 1 && (
+          <>
+            <h3>User Information</h3>
+            <TextField
+              id="numEmp-basic"
+              label="N° EMP"
+              variant="standard"
+              name="numEMP"
+              value={userFormData.numEMP}
+              onChange={handleUserInputChange}
+              style={{ marginBottom: '10px' }}
+              required
+            />
+            <TextField
+              id="userUsername-basic"
+              label="Username"
+              variant="standard"
+              name="userUsername"
+              value={userFormData.userUsername}
+              onChange={handleUserInputChange}
+              style={{ marginBottom: '10px' }}
+              required
+            />
+            <TextField
+              id="userPassword-basic"
+              label="Password"
+              variant="standard"
+              name="userPassword"
+              value={userFormData.userPassword}
+              onChange={handleUserInputChange}
+              style={{ marginBottom: '10px' }}
+              required
+            />
 
-    </div>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Button
+                variant="contained"
+                color="success"
+                onClick={handlePrevious}
+              >
+                Previous
+              </Button>
+              <Button
+                variant="contained"
+                color="success"
+                onClick={handleSubmit}
+                disabled={Object.values(userFormData).some((value) => value === '')}
+              >
+                Submit
+              </Button>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
